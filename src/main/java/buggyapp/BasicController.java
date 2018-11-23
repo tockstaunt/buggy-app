a50f2fd7e6ea150f28f0d1085d0f0dc9dadea0b3
Keith Collier <34369272+tockstaunt@users.noreply.github.com>
2018-11-22 23:31:56 -0600

    added random number generator

--- src/main/java/buggyapp/BasicController.java
+++ src/main/java/buggyapp/BasicController.java
@@ -1,6 +1,7 @@
 package buggyapp;
 
 import java.time.LocalDateTime;
+import java.util.Random;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,14 @@ public class BasicController {
 		LocalDateTime now = LocalDateTime.now();
 		result = now.toString();
 		System.out.println(result);
+		return result;
+	}
+	
+	@RequestMapping("/random")
+	public @ResponseBody String random() {
+		String result = "";
+		Random r = new Random();		
+		result = Integer.toString(r.nextInt());
 		return result;
 	}
 
